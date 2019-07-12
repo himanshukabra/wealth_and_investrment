3c341c89b8da1af6769e33dfeee7d9859dc41e9f
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-12 13:07:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_restful import Resource, Api 
 app = Flask(__name__)
-
+api = Api(app) 
 @app.route("/")
-def hello():
-    return "Hello Himanshu"
+#def hello():
+#    return "Hello Himanshu"
+
+class api_status(Resource):
+    def get(self):
+        '''
+        The aim of this function is to check if the API is up
+        '''
+        return 'http 200'
+
+api.add_resource(api_status, '/healthcheck')
