cbdbec10f5a53209204532e8ca8d5be3e3005f01
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-14 19:44:01 +0530

    Update application.py

--- application.py
+++ application.py
@@ -116,7 +116,7 @@ def get_ledger_master():
 
     return json_final_data
 
-@app.route("/get_ledger")
+@app.route('/get_ledger', methods=['POST'])
 def get_ledger():
     
        import pyodbc
@@ -146,6 +146,44 @@ def get_ledger():
               print(e)
 
           query = "exec USP_R_Ledger 1,%s,%s,%s,%s"%(data['ledgerhead'],data['ledgeraccount'],data['from_date'],data['to_date'])
+
+          abc = pd.read_sql(query, conn)    
+          json_final_data = abc.to_json(orient='records', date_format = 'iso')
+
+          conn.close()
+
+       else:
+
+          json_final_data = jsonify({"message": "ERROR: Unauthorized Access"}), 401
+
+       return json_final_data
+
+@app.route('/get_trial_balance', methods=['POST'])
+def get_trial_balance():
+    
+       import pyodbc
+       import pandas as pd
+       import pandas.io.sql as psql
+       from flask import Flask, request, jsonify
+
+       headers = request.headers
+       auth = headers.get("X-Api-Key")
+       if auth == 'asoidewfoef':  
+
+          data = []
+          data = {'dbname':request.json['dbname']}
+
+          db=data['dbname']
+          user="shsa"
+          server="13.127.124.84,6016"
+          password="Easeprint#021"
+          port = "80"
+          try:
+              conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+          except Exception as e:
+              print(e)
+
+          query = "exec Usp_R_TrialBalaceNav 0"
 
           abc = pd.read_sql(query, conn)    
           json_final_data = abc.to_json(orient='records', date_format = 'iso')
