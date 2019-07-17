2976cf31c99bc8dd60551e0591f7666c4162c26a
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-17 22:12:32 +0530

    Update application.py

--- application.py
+++ application.py
@@ -410,30 +410,36 @@ def get_gp_data_cash_book():
     import pandas as pd
     import pandas.io.sql as psql
     from flask import Flask, request, jsonify
-      
-    data = []
-    data = {'dbname':request.json['dbname']}
+     
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth == 'asoidewfoef':  
+  
+       data = []
+       data = {'dbname':request.json['dbname']}
 
-    db=data['dbname']
-    user="shsa"
-    server="13.127.124.84,6016"
-    password="Easeprint#021"
-    port = "80"
-    try:
-        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
-    except Exception as e:
-        print(e)
+       db=data['dbname']
+       user="shsa"
+       server="13.127.124.84,6016"
+       password="Easeprint#021"
+       port = "80"
+       try:
+           conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+       except Exception as e:
+           print(e)
 
-    #query = "select * from m_main_ledger_head"
-    query = "select LedgerHeadId,LedgerId from GlobalPreffrences where UsedHead='CashBook'"
+       #query = "select * from m_main_ledger_head"
+       query = "select LedgerHeadId,LedgerId from GlobalPreffrences where UsedHead='CashBook'"
 
-    abc = pd.read_sql(query, conn)    
-    json_final_data = abc.to_json(orient='records', date_format = 'iso')
+       abc = pd.read_sql(query, conn)    
+       json_final_data = abc.to_json(orient='records', date_format = 'iso')
 
-    conn.close() 
-
-    return json_final_data  
-
+       conn.close() 
+       return json_final_data  
+    else:           
+       json_final_data = jsonify({"message": "ERROR: Unauthorized Access"}), 401
+       return json_final_data 
+   
 @app.route('/get_cash_opening_balance', methods=['POST'])
 def get_gp_data_cash_book():
     
