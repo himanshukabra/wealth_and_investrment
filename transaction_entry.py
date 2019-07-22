6c88455ce805aea71e1d41af3420d123a9ec459f
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-22 15:10:28 +0530

    Update transaction_entry.py

--- transaction_entry.py
+++ transaction_entry.py
@@ -25,3 +25,31 @@ def get_product(dbname):
     conn.close() 
 
     return json_final_data
+
+def get_scheme_list(dbname,product_id):
+    
+    import pyodbc
+    import pandas as pd
+    import pandas.io.sql as psql
+    from flask import Flask, request, jsonify
+      
+    db=dbname
+    user="shsa"
+    server="13.127.124.84,6016"
+    password="Easeprint#021"
+    port = "80"
+    try:
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+
+    except Exception as e:
+        print(e)
+
+    query = "select * from (select Table_Id,ScripName,Product_Id from M_Equity union select id as tableid,bond_name as scripname,3 as Product_Id from M_Bonds union select Table_Id,Scheme_Name as scripname,Product_Id from M_Mutual_Funds) t where Product_Id = %s"%product_id
+
+    abc = pd.read_sql(query, conn)
+
+    json_final_data = abc.to_json(orient='records', date_format = 'iso')
+
+    conn.close() 
+
+    return json_final_data
