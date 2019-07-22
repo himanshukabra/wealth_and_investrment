be96cb5f5962a878548cb4b5bb8acb4638874382
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-22 18:07:34 +0530

    Update transaction_entry.py

--- transaction_entry.py
+++ transaction_entry.py
@@ -100,7 +100,7 @@ def get_demat(dbname,broker_id):
     except Exception as e:
         print(e)
 
-    query = "select broker_id,id,demat_account_number from m_demat_accounts where broker_id = %s union all select 0 as broker_id,0 as id,0  as demat_Account_number"%broker_id
+    query = "Select top 1 broker_id,id,demat_account_number from (select broker_id,id,demat_account_number from m_demat_accounts where broker_id = %s union all select 0 as broker_id,0 as id,0  as demat_Account_number) t order by t.broker_id desc"%broker_id
 
     abc = pd.read_sql(query, conn)
 
@@ -128,7 +128,7 @@ def get_scrip_opening(dbname,scrip_id,product_id,folio):
     except Exception as e:
         print(e)
 
-    query = "select product_id,scrip_Id,folio_number,sum(quantity) as closing_quanity from ( select product_id,script_id as scrip_Id,isnull(folio_number,0) as folio_number, sum(case when transaction_type = 'Buy' then quantity when transaction_type = 'Sell' then -quantity end) as quantity from t_transaction_register where script_id = %s and product_id = %s and folio_number = '%s' group by product_id,script_id,isnull(folio_number,0) union select product_id,scrip_Id,isnull(folio_number,'0') as folio_number,sum(quantity) as quantity from t_opening_balance_products where scrip_id = %s and product_id = %s and folio_number = '%s' group by product_id,scrip_Id,isnull(folio_number,'0') ) t group by product_id,scrip_Id,folio_number"%(scrip_id,product_id,folio,scrip_id,product_id,folio)
+    query = "select top 1 * from (select product_id,scrip_Id,folio_number,sum(quantity) as closing_quanity from ( select product_id,script_id as scrip_Id,isnull(folio_number,0) as folio_number, sum(case when transaction_type = 'Buy' then quantity when transaction_type = 'Sell' then -quantity end) as quantity from t_transaction_register where script_id = %s and product_id = %s and folio_number = '%s' group by product_id,script_id,isnull(folio_number,0) union select product_id,scrip_Id,isnull(folio_number,'0') as folio_number,sum(quantity) as quantity from t_opening_balance_products where scrip_id = %s and product_id = %s and folio_number = '%s' group by product_id,scrip_Id,isnull(folio_number,'0') ) t group by product_id,scrip_Id,folio_number) t order by scrip_Id desc"%(scrip_id,product_id,folio,scrip_id,product_id,folio)
 
     abc = pd.read_sql(query, conn)
 
