5acd4c679af8f222ece40e24baaeaf769d9fe3dd
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-28 14:24:33 +0530

    Update transaction_entry.py

--- transaction_entry.py
+++ transaction_entry.py
@@ -137,3 +137,33 @@ def get_scrip_opening(dbname,scrip_id,product_id,folio):
     conn.close() 
 
     return json_final_data
+
+def get_folios(dbname,scrip_id,product_id):
+    
+    import pyodbc
+    import pandas as pd
+    import pandas.io.sql as psql
+    from flask import Flask, request, jsonify
+      
+    db=dbname
+    user="shsa"
+    server="13.127.124.84,6016"
+    password="Easeprint#021"
+    port = "80"
+    try:
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+
+    except Exception as e:
+        print(e)
+
+    query = "select distinct product_id,scrip_id,folio_number from (select product_id,Bond_Scrip_Id as scrip_id,folio_number from T_Opening_Balance_Products union select product_id,script_id as scrip_id,folio_number from t_transaction_register ) t where folio_number is not null and product_id = %s and scrip_id = %s"%(product_id,scrip_id)
+
+    abc = pd.read_sql(query, conn)
+
+    json_final_data = abc.to_json(orient='records', date_format = 'iso')
+
+    conn.close() 
+
+    return json_final_data
+
+
