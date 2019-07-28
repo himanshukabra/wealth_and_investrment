3133cb286384e2838bf91594b67586a02f8b9c9c
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-28 22:33:22 +0530

    Update transaction_entry.py

--- transaction_entry.py
+++ transaction_entry.py
@@ -197,3 +197,31 @@ def insert_temp_transaction_register(dbname,product_id,scrip_id,folio_number,tra
 
     conn.close() 
     return jsonify({"message": val}), 200
+
+def get_temp_transaction(dbname,user,computer_name):
+    
+    import pyodbc
+    import pandas as pd
+    import pandas.io.sql as psql
+    from flask import Flask, request, jsonify
+      
+    db=dbname
+    user="shsa"
+    server="13.127.124.84,6016"
+    password="Easeprint#021"
+    port = "80"
+    try:
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+
+    except Exception as e:
+        print(e)
+
+    query = "select * from t_transaction_api_temp where user = '%s' and computer_name = '%s'"%(user,computer_name)
+
+    abc = pd.read_sql(query, conn)
+
+    json_final_data = abc.to_json(orient='records', date_format = 'iso')
+
+    conn.close() 
+
+    return json_final_data
