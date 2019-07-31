8a1e270c2903d82d2c87e2cd58c72d4d3f7a71dd
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-07-31 15:26:44 +0530

    Update transaction_entry.py

--- transaction_entry.py
+++ transaction_entry.py
@@ -277,7 +277,7 @@ def get_total_for_temp_transaction(dbname,username,computer_name):
     except Exception as e:
         print(e)
     cur = conn.cursor()
-    query = "select sum(isnull(gross_amount,0)) as total_gross_amount,sum(isnull(brokerage,0)) as total_brokerage, sum(isnull(stt,0)) as total_stt from t_transaction_api_temp where user = '%s' and computer_name = '%s'"%(username,computer_name)
+    query = "select isnull(sum(isnull(gross_amount,0)),0) as total_gross_amount,isnull(sum(isnull(brokerage,0)),0) as total_brokerage, isnull(sum(isnull(stt,0)),0) as total_stt from t_transaction_api_temp where user = '%s' and computer_name = '%s'"%(username,computer_name)
 
     abc = pd.read_sql(query, conn)
 
