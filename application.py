ea5b1f7e0584135f440e6ca4aff112b88860d2dd
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-08-02 11:16:24 +0530

    Update application.py

--- application.py
+++ application.py
@@ -825,7 +825,7 @@ def update_account_transaction_for_transaction_entry():
        if float(data['other_charges_amount'])>0:
             json_final_data = insert_data_in_at_from_transaction_entry(data['dbname'],3,data['date'],0,0,'',data['contract_number'],'Other Transaction Tax on contract number '+str(data['contract_number']),abs(float(data['other_charges_amount'])),data['broker_head'],data['broker_ledger'],data['computer_name'],data['createdby'])
 
-       data_from_db = get_temp_data_from_transaction_register(data['dbname'],data['user_name'],data['computer_name'])
+       data_from_db = get_temp_transaction(data['dbname'],data['user_name'],data['computer_name'])
             
        for i in data_from_db.itertuples():
            json_final_data = insert_data_in_transaction_register(data['dbname'],data['date'],data['broker_id'],data['demat_id'],data['contract_number'],data['reference_number'],i[1],i[2],i[3],i[5],i[6],i[7],i[8],i[9],i[10],i[11],i[8]+i[9]+i[10],'web',data['user_name'],data['remarks'])
