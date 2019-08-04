30c423378ed0d5a8fa705a129e223dd38c12cf01
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-08-04 17:51:56 +0530

    Update auto_transaction_entry.py

--- auto_transaction_entry.py
+++ auto_transaction_entry.py
@@ -57,3 +57,31 @@ def update_auto_debit_transaction(dbname,amount_paid,bank_ledger,paid_on,table_i
 
     conn.close() 
     return val
+
+def get_bank_names(dbname):
+
+    import pyodbc
+    import pandas as pd
+    import pandas.io.sql as psql
+    from flask import Flask, request, jsonify
+      
+    db=dbname
+    user="shsa"
+    server="13.127.124.84,6016"
+    password="Easeprint#021"
+    port = "80"
+    try:
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+
+    except Exception as e:
+        print(e)
+
+    query = "select ledgername,tableid from m_ledgermaster where ledgerhead = 17"
+
+    abc = pd.read_sql(query, conn)
+
+    conn.close()  
+
+    json_final_data = abc.to_json(orient='records', date_format = 'iso')
+    
+    return json_final_data
