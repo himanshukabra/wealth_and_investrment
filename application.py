af5879649b09ed3fa4e0419a7b711a88b2172d51
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-08-25 21:03:53 +0530

    Update application.py

--- application.py
+++ application.py
@@ -976,10 +976,10 @@ def test():
                }
 
                r = requests.post(url,data=(mf_code_string), headers=param)
+               
+               mf_nav_data =r.content
 
-               abc =r.content
-
-               return abc
+               return mf_nav_data
 
            def calculate_current_value(row):
 
@@ -1020,8 +1020,7 @@ def test():
            for i in scehme_codes:
                scheme_code_stirng = scheme_code_stirng + '"' + str(i) + '"' + ","
            mf_code_string = replace_last(scheme_code_stirng, ',', ']}')  
-           a = scehme_codes.to_dict()
-#            a = json.loads(get_mutual_fund_nav(mf_code_string))
+           a = json.loads(get_mutual_fund_nav(mf_code_string))
 #            ab = a['data']
 #            mf_nav_from_site = pd.DataFrame.from_dict(ab)
 #            mf_nav_from_site["schemeCode"] = pd.to_numeric(mf_nav_from_site["schemeCode"])
@@ -1035,4 +1034,4 @@ def test():
 #            mf_final_data['gain/loss'] = mf_final_data.apply(calculate_gain_loss, axis =1) 
 #            mf_final_data['absolute_gain(%)'] = mf_final_data.apply(calculate_absolute_gain, axis =1) 
 #            mf_final_data = mf_final_data.round({'total_amount' : 2,'current_value' : 2,'gain/loss' : 2,'absolute_gain(%)':2})
-           return mf_code_string
+           return a
