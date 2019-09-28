a4c4f8c39bf99b42513291fae56afb6d2472e95b
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-09-28 19:27:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -880,9 +880,9 @@ def post_auto_transaction_entry():
                'scrip_id':request.json['scrip_id'],
                'folio_number':request.json['folio_number']}
         
-        json_final_data = insert_data_in_at_from_transaction_entry(data['dbname'],0,data['date'],0,data['investment_in_ledger'],'',data['auto_debit_table_id'],'PURCHASE OF ' + str(data['scrip_name'])+' - UNITS - '+str(data['units']),abs(float(data['gross_amount'])),0,data['bank_ledger'],data['computer_name'],data['created_by'])           
-        json_final_data = insert_final_data_in_transaction_register(data['dbname'],data['date'],data['broker_id'],0,data['auto_debit_table_id'],'auto_debit_transaction',data['product_id'],data['scrip_id'],data['folio_number'],'Buy',data['units'],data['gross_rate'],data['gross_amount'],0,0,0,data['gross_amount'],'web',data['created_by'],'auto_debit_transaction')
-        update_auto_debit_transaction(data['dbname'],data['gross_amount'],data['bank_ledger'],data['date'],data['auto_debit_table_id'])      
+       json_final_data = insert_data_in_at_from_transaction_entry(data['dbname'],0,data['date'],0,data['investment_in_ledger'],'',data['auto_debit_table_id'],'PURCHASE OF ' + str(data['scrip_name'])+' - UNITS - '+str(data['units']),abs(float(data['gross_amount'])),0,data['bank_ledger'],data['computer_name'],data['created_by'])           
+       json_final_data = insert_final_data_in_transaction_register(data['dbname'],data['date'],data['broker_id'],0,data['auto_debit_table_id'],'auto_debit_transaction',data['product_id'],data['scrip_id'],data['folio_number'],'Buy',data['units'],data['gross_rate'],data['gross_amount'],0,0,0,data['gross_amount'],'web',data['created_by'],'auto_debit_transaction')
+       update_auto_debit_transaction(data['dbname'],data['gross_amount'],data['bank_ledger'],data['date'],data['auto_debit_table_id'])      
 
     else:           
        json_final_data = "ERROR: Unauthorized Access"
