138c9562ff2564d3d8876286a1ee205c9d5be7c0
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2019-10-13 19:34:29 +0530

    Update cash_book.py

--- cash_book.py
+++ cash_book.py
@@ -94,3 +94,32 @@ def delete_cash_book_entry(dbname,table_id,auto_serial_number):
 
     conn.close() 
     return jsonify({"message": val}), 200   
+
+def get_bank_book_data_for_auto_serial_number(dbname,auto_serial_number):
+    
+    import pyodbc
+    import pandas as pd
+    import pandas.io.sql as psql
+    import json
+    from flask import Flask, request, jsonify
+    pd.options.mode.chained_assignment = None    
+
+    db=dbname
+    user="shsa"
+    server="13.127.124.84,6016"
+    password="Easeprint#021"
+    port = "80"
+    try:
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+    except Exception as e:
+        print(e)
+
+    cur = conn.cursor()
+
+    query = "select t1.tableid,t1.autoserialnumber,t1.date,t1.accounthead,t1.accountledger,t2.ledgername as account_ledger_name,t3.mainledgerheads as account_head_name,t1.drcr,t1.vouchernumber,t1.standarddescription1,t1.amount,t1.ledgerhead1,t1.ledger1,t4.ledgername as bank_ledger_name,t5.mainledgerheads as bank_head_name from t_bankbook t1 left join m_ledgermaster t2 on t1.accountledger = t2.tableid left join M_Main_Ledger_Head t3 on t1.accounthead = t3.mainleadgerheadid left join m_ledgermaster t4 on t1.ledger1 = t4.tableid left join M_Main_Ledger_Head t5 on t1.ledgerhead1 = t5.mainleadgerheadid where autoserialnumber = %s"%(auto_serial_number)    
+    abc = pd.read_sql(query, conn)
+    conn.close() 
+    
+    json_final_data = abc.to_json(orient='records', date_format = 'iso')
+    
+    return json_final_data 
