f2babb9f1eea11e61802cfadd9e3bff3a2c56867
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2020-04-05 19:02:39 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1420,6 +1420,7 @@ def get_mutual_fund_holdings():
            #### mutual fund scheme calculation
            mutual_fund_data = data1.loc[data1['product_name']=='Mutual Fund']    
            scehme_codes = mutual_fund_data['scrip_code']
+           print(scehme_codes)
            scheme_code_stirng = '{'+ '"' + 'schemeCodes' + '"' + ":["
            for i in scehme_codes:
                scheme_code_stirng = scheme_code_stirng + '"' + str(i[:6]) + '"' + ","
