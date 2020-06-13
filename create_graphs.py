1d7c24fcb26c765f43fb4a084bba695cd250fedd
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2020-06-13 17:32:26 +0530

    Create create_graphs.py

---
+++ create_graphs.py
@@ -0,0 +1,25 @@
+import pyodbc
+import pandas as pd
+import pandas.io.sql as psql
+import json
+import datetime as dt
+from pandas.io.json import json_normalize
+from flask import Flask, request, jsonify
+pd.options.mode.chained_assignment = None
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import warnings
+warnings.filterwarnings("ignore")
+
+user="shsa"
+server="103.212.121.67"
+password="Easeprint#021"
+
+def get_investment_graph():
+
+
+
+
+
+    return 
