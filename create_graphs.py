11efec242534c9104889f4b79b1f788a63c4f7e3
himanshukabra <52815683+himanshukabra@users.noreply.github.com>
2020-06-13 17:37:30 +0530

    Update create_graphs.py

--- create_graphs.py
+++ create_graphs.py
@@ -16,10 +16,30 @@ user="shsa"
 server="103.212.121.67"
 password="Easeprint#021"
 
-def get_investment_graph():
+def get_investment_graph(db_name):
 
+    try:
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db_name+';UID='+user+';PWD='+ password)
+    except Exception as e:
+        print(e)
 
+    query = "exec Usp_R_Queries_for_Charts 0"
 
+    abc = pd.read_sql(query, conn)
 
-
-    return 
+    conn.close() 
+    
+    def create_figure(abc):
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        ax1 = plt.subplot(121, aspect='equal')
+        plot = abc.plot(kind='pie', y='percentage_investment', ax=ax1, figsize=(20,20),autopct='%1.0f%%', 
+               startangle=180, shadow=False, labels=abc['investment_type'], legend = False, fontsize=9)
+        plt.legend(bbox_to_anchor=(1.2, 1), loc=1, borderaxespad=0)
+        plt.axis('off')
+        return fig
+    
+    fig = create_figure(abc)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')    
